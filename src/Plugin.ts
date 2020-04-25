6f43c55865427195aa1e123a4642bb7702414481
chroventer <ayyanlewis@gmail.com>
2020-04-25 10:30:43 -0700

    style: lint the source files

--- src/Plugin.ts
+++ src/Plugin.ts
@@ -1,27 +1,11 @@
-import { Socket, Player } from "./index";
-import { Manager } from "./Manager";
-import { SocketOptions } from "./Socket";
+import {Socket, Player} from '.';
+import {Manager} from './Manager';
+import {SocketOptions} from './Socket';
 
-export class Plugin {
-  public manager: Manager;
-
-  public onLoad(): any {
-    throw new Error(`${this.constructor.name}#onLoad hasn't been implemented.`);
-  }
-
-  public onNewSocket(_socket: Socket, _options: SocketOptions): any {
-    throw new Error(
-      `${this.constructor.name}#onNewSocket hasn't been implemented.`
-    );
-  }
-
-  public onPlayerSummon(_player: Player): any {
-    throw new Error(
-      `${this.constructor.name}#onPlayerSummon hasn't been implemented.`
-    );
-  }
-
-  public init(manager: Manager) {
-    this.manager = manager;
-  }
+export abstract class Plugin {
+	public manager: Manager;
+	public abstract onLoad(): any;
+	public abstract onNewSocket(_socket: Socket, _options: SocketOptions): any;
+	public abstract onPlayerSummon(_player: Player): any;
+	public abstract init(manager: Manager): void;
 }
