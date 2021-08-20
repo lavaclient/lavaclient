f07160c434ee7691b802e59fe2faecc506119400
melike2d <gino@dimensional.fun>
2021-08-20 10:47:12 -0700

    format entire proj

--- src/structures/Plugin.ts
+++ src/structures/Plugin.ts
@@ -1,26 +1,26 @@
 import type { Manager } from "./Manager";
 
 export abstract class Plugin {
-  /**
-   * The manager that loaded this plugin.
-   */
-  manager!: Manager;
+    /**
+     * The manager that loaded this plugin.
+     */
+    manager!: Manager;
 
-  /**
-   * Called when this plugin is loaded.
-   * @param manager The manager that loaded this plugin.
-   * @since 3.0.0
-   */
-  load(manager: Manager): void {
-    this.manager = manager;
-    return;
-  }
+    /**
+     * Called when this plugin is loaded.
+     * @param manager The manager that loaded this plugin.
+     * @since 3.0.0
+     */
+    load(manager: Manager): void {
+        this.manager = manager;
+        return;
+    }
 
-  /**
-   * Called when the manager is initialized.
-   * @since 3.0.0
-   */
-  init(): void {
-    return;
-  }
+    /**
+     * Called when the manager is initialized.
+     * @since 3.0.0
+     */
+    init(): void {
+        return;
+    }
 }
