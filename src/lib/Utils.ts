fc879ee98525a93a4a966bbd6cdf60e8fbfa5db2
melike2d <gino@dimensional.fun>
2021-08-20 19:05:04 -0700

    :fire: make the Manager interface useless lol

--- src/lib/Utils.ts
+++ src/lib/Utils.ts
@@ -1,5 +1,3 @@
-import type { Player, VoiceServerUpdate, VoiceStateUpdate } from "./Player";
-import type { REST } from "./node/REST";
 import type { SendGatewayPayload } from "./node/Node";
 
 export type Snowflake = string;
@@ -7,15 +5,6 @@ export type DiscordResource = { id: Snowflake };
 export type Dictionary<V = any, K extends string | symbol = string> = Record<K, V>;
 
 export interface Manager {
-    readonly rest: REST;
-    readonly sendGatewayPayload: SendGatewayPayload;
-
-    userId?: Snowflake;
-
-    connect(user?: Snowflake | DiscordResource): void;
-    createPlayer(guild: Snowflake | DiscordResource): Player;
-    destroyPlayer(guild: Snowflake | DiscordResource): boolean;
-    handleVoiceUpdate(update: VoiceStateUpdate | VoiceServerUpdate): void;
 }
 
 export interface ManagerOptions {
