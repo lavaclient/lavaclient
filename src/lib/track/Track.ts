e2fbdb8e984bd4beae58011f1aeaff97ba55ba07
melike2d <ml2dofficial@gmail.com>
2022-04-03 16:06:35 -0700

    :sparkles: improve track decoding

--- src/lib/track/Track.ts
+++ src/lib/track/Track.ts
@@ -3,7 +3,7 @@ import { DataInput } from "./DataInput";
 export const TRACK_INFO_VERSIONED = 1, TRACK_INFO_VERSION = 2;
 
 export const decoders: Record<number, TrackInfoDecoder> = { 
-    2: (input: DataInput) => {
+    2: input => {
         const track: Partial<TrackInfo> = { 
             title:      input.readUTF(), 
             author:     input.readUTF(), 
@@ -15,6 +15,12 @@ export const decoders: Record<number, TrackInfoDecoder> = {
             version:    2
         };
 
+        if (["local", "http"].includes(track.sourceName ?? "")) {
+            const [name, parameters] = input.readUTF().split("|");
+            track.probeInfo = { name, parameters };
+        }
+
+        track.position = Number(input.readLong());
         track.isSeekable = !track.isStream;
         return track as TrackInfo;
     },
@@ -27,9 +33,15 @@ export function decode(data: Uint8Array | string): TrackInfo | null {
 }
 
 function readVersion(input: DataInput): number {
-    const flags = input.readInt() >> 30;
+    const flags = (input.readInt() & 0xC0000000) >> 30;
     return flags & TRACK_INFO_VERSIONED ? input.readByte() : 1;
 }
 
-export type TrackInfo = import("@lavaclient/types").TrackInfo & { version: number }
+export type TrackInfo = import("@lavaclient/types").TrackInfo & { 
+    version: number;
+    probeInfo: { 
+        name: string;
+        parameters?: string;
+    }
+}
 export type TrackInfoDecoder = (input: DataInput) => TrackInfo | null;
